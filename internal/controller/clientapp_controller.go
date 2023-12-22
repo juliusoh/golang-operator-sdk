2edd999e4f0257cfa85d948a038a1a20810114fc
Julius Oh <juliusoh@gmail.com>
2023-12-22 10:56:16 -0800

    Clinia Test Operator

--- internal/controller/clientapp_controller.go
+++ internal/controller/clientapp_controller.go
@@ -27,8 +27,10 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
@@ -93,12 +95,50 @@ func (r *ClientAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 	}
 
 	// Update ClientAppStatus Available
+	// Todo: monitor deployment itself, pods are running properly (check for img pull err), check for container startup errors
+	// Todo: run operator in cluster and change replicas to verify changes
+	// Todo: status resource -> valid and makes sense
 	clientApp.Status.Available = true
 	// Update ClientAppStatus URL
 	service := &corev1.Service{}
+	err = r.Get(ctx, types.NamespacedName{Name: clientApp.Name, Namespace: clientApp.Namespace}, service)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			dep := r.serviceForClientApp(clientApp)
+			if err := r.Create(ctx, dep); err != nil {
+				return ctrl.Result{}, err
+			}
+			return ctrl.Result{Requeue: true}, nil
+		} else {
+			return ctrl.Result{}, err
+		}
+	}
 	clientApp.Status.URL = "http://" + service.Status.LoadBalancer.Ingress[0].IP
 
 	return ctrl.Result{}, nil
+}
+
+func (r *ClientAppReconciler) serviceForClientApp(m *cachev1alpha1.ClientApp) *corev1.Service {
+	lbls := labelsForApp(m.Name)
+
+	svc := &corev1.Service {
+		ObjectMeta: metav1.ObjectMeta{
+			Name: m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: corev1.ServiceSpec{
+			Selector: lbls,
+			Type: corev1.ServiceTypeLoadBalancer,
+			Ports: []corev1.ServicePort{{
+				Port: m.Spec.Port,
+				TargetPort: intstr.FromInt(int(m.Spec.Port)),
+				Protocol: corev1.ProtocolTCP,
+				Name: "clientapp",
+			}},
+	},
+}
+	controllerutil.SetControllerReference(m, svc, r.Scheme)
+	return svc
 }
 
 func (r *ClientAppReconciler) deploymentForClientApp(m *cachev1alpha1.ClientApp) *appsv1.Deployment {
@@ -151,5 +191,7 @@ func labelsForApp(name string) map[string]string {
 func (r *ClientAppReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&cachev1alpha1.ClientApp{}).
+		Owns(&appsv1.Deployment{}).
+		WithOptions(controller.Options{MaxConcurrentReconciles: 2}).
 		Complete(r)
 }
