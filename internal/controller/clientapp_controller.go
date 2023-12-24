7716bc4498b1702e71d444670258f731eef33882
Julius Oh <juliusoh@gmail.com>
2023-12-24 14:46:08 -0800

    Add validation for deployment and ingress logic

--- internal/controller/clientapp_controller.go
+++ internal/controller/clientapp_controller.go
@@ -23,6 +23,7 @@ import (
 	cachev1alpha1 "github.com/juliusoh/clinia-test/api/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -32,7 +33,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
-	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 // ClientAppReconciler reconciles a ClientApp object
@@ -56,67 +56,110 @@ type ClientAppReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
 func (r *ClientAppReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+    // Retrieve ClientApp instance
+    clientApp := &cachev1alpha1.ClientApp{}
+    if err := r.Get(ctx, req.NamespacedName, clientApp); err != nil {
+        if errors.IsNotFound(err) {
+            // Object not found, return.  Created objects are automatically garbage collected.
+            return ctrl.Result{}, nil
+        }
+        // Error reading the object - requeue the request.
+        return ctrl.Result{}, err
+    }
 
-	// TODO(user): your logic here
-	// Retrieve Client App Instance
-	clientApp := &cachev1alpha1.ClientApp{}
-	err := r.Get(ctx, req.NamespacedName, clientApp)
+    // Reconcile Deployment
+    deployment := &appsv1.Deployment{}
+    if err := r.Get(ctx, types.NamespacedName{Name: clientApp.Name, Namespace: clientApp.Namespace}, deployment); err != nil {
+        if errors.IsNotFound(err) {
+            // Define a new deployment
+            dep := r.deploymentForClientApp(clientApp)
+            if err := r.Create(ctx, dep); err != nil {
+                return ctrl.Result{}, err
+            }
+            // Deployment created successfully - return and requeue
+            return ctrl.Result{Requeue: true}, nil
+        } else {
+            // Error reading the deployment - requeue the request.
+            return ctrl.Result{}, err
+        }
+    }
+
+    // Update Deployment if replicas count doesn't match
+    if *deployment.Spec.Replicas != clientApp.Spec.Replicas {
+        deployment.Spec.Replicas = &clientApp.Spec.Replicas
+        if err := r.Update(ctx, deployment); err != nil {
+            return ctrl.Result{}, err
+        }
+        // Deployment updated successfully - return and requeue
+        return ctrl.Result{Requeue: true}, nil
+    }
+
+    // Update ClientApp Status based on the Deployment's status
+    if deployment.Status.AvailableReplicas == clientApp.Spec.Replicas {
+        if !clientApp.Status.Available {
+            clientApp.Status.Available = true
+            if err := r.Status().Update(ctx, clientApp); err != nil {
+                return ctrl.Result{}, err
+            }
+        }
+    } else {
+        if clientApp.Status.Available {
+            clientApp.Status.Available = false
+            if err := r.Status().Update(ctx, clientApp); err != nil {
+                return ctrl.Result{}, err
+            }
+        }
+    }
+
+    // Reconcile Service
+    service := &corev1.Service{}
+    if err := r.Get(ctx, types.NamespacedName{Name: clientApp.Name, Namespace: clientApp.Namespace}, service); err != nil {
+        if errors.IsNotFound(err) {
+            // Define a new service
+            svc := r.serviceForClientApp(clientApp)
+            if err := r.Create(ctx, svc); err != nil {
+                return ctrl.Result{}, err
+            }
+            // Service created successfully - return and requeue
+            return ctrl.Result{Requeue: true}, nil
+        } else {
+            // Error reading the service - requeue the request.
+            return ctrl.Result{}, err
+        }
+    }
+
+	// Reconcile Ingress
+	ingress := &networkingv1.Ingress{}
+	err := r.Get(ctx, types.NamespacedName{Name: clientApp.Name, Namespace: clientApp.Namespace}, ingress)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			return ctrl.Result{}, nil
-		}
-		return ctrl.Result{}, err
-	}
-
-	// Check if deployment exists or else create new deployment
-	found := &appsv1.Deployment{}
-	err = r.Get(ctx, types.NamespacedName{Name: clientApp.Name, Namespace: clientApp.Namespace}, found)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			dep := r.deploymentForClientApp(clientApp)
-			if err := r.Create(ctx, dep); err != nil {
+			// Define a new ingress
+			ing := r.ingressForClientApp(clientApp, service) 
+			if err := r.Create(ctx, ing); err != nil {
 				return ctrl.Result{}, err
 			}
+			// Ingress created successfully - return and requeue
 			return ctrl.Result{Requeue: true}, nil
 		} else {
+			// Error reading the ingress - requeue the request.
 			return ctrl.Result{}, err
 		}
 	}
 
-	// Ensure the deployment size is the same as the spec
-	size := clientApp.Spec.Replicas 
-	if *found.Spec.Replicas != size {
-		found.Spec.Replicas = &size
-		if err := r.Update(ctx, found); err != nil {
-			return ctrl.Result{}, err
-		}
-		return ctrl.Result{Requeue: true}, nil
-	}
+    // Update ClientApp Status URL if it's different
+    if len(service.Status.LoadBalancer.Ingress) > 0 {
+        url := "http://" + service.Status.LoadBalancer.Ingress[0].IP
+        if clientApp.Status.URL != url {
+            clientApp.Status.URL = url
+            if err := r.Status().Update(ctx, clientApp); err != nil {
+                return ctrl.Result{}, err
+            }
+        }
+    }
 
-	// Update ClientAppStatus Available
-	// Todo: monitor deployment itself, pods are running properly (check for img pull err), check for container startup errors
-	// Todo: run operator in cluster and change replicas to verify changes
-	// Todo: status resource -> valid and makes sense
-	clientApp.Status.Available = true
-	// Update ClientAppStatus URL
-	service := &corev1.Service{}
-	err = r.Get(ctx, types.NamespacedName{Name: clientApp.Name, Namespace: clientApp.Namespace}, service)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			dep := r.serviceForClientApp(clientApp)
-			if err := r.Create(ctx, dep); err != nil {
-				return ctrl.Result{}, err
-			}
-			return ctrl.Result{Requeue: true}, nil
-		} else {
-			return ctrl.Result{}, err
-		}
-	}
-	clientApp.Status.URL = "http://" + service.Status.LoadBalancer.Ingress[0].IP
-
-	return ctrl.Result{}, nil
+    return ctrl.Result{}, nil
 }
+
 
 func (r *ClientAppReconciler) serviceForClientApp(m *cachev1alpha1.ClientApp) *corev1.Service {
 	lbls := labelsForApp(m.Name)
@@ -181,6 +224,49 @@ func (r *ClientAppReconciler) deploymentForClientApp(m *cachev1alpha1.ClientApp)
     controllerutil.SetControllerReference(m, dep, r.Scheme)
     return dep
 }
+
+func (r *ClientAppReconciler) ingressForClientApp(m *cachev1alpha1.ClientApp, service *corev1.Service) *networkingv1.Ingress {
+    pathType := networkingv1.PathTypeImplementationSpecific 
+
+    ingress := &networkingv1.Ingress{
+        ObjectMeta: metav1.ObjectMeta{
+            Name: m.Name,
+            Namespace: m.Namespace,
+            Annotations: map[string]string{
+                "nginx.ingress.kubernetes.io/rewrite-target": "/",
+            },
+        },
+        Spec: networkingv1.IngressSpec{
+            Rules: []networkingv1.IngressRule{
+                {
+                    Host: m.Spec.Host,
+                    IngressRuleValue: networkingv1.IngressRuleValue{
+                        HTTP: &networkingv1.HTTPIngressRuleValue{
+                            Paths: []networkingv1.HTTPIngressPath{
+                                {
+                                    Path: "/",
+                                    PathType: &pathType, 
+                                    Backend: networkingv1.IngressBackend{
+                                        Service: &networkingv1.IngressServiceBackend{
+                                            Name: service.Name,
+                                            Port: networkingv1.ServiceBackendPort{
+                                                Number: m.Spec.Port,
+                                            },
+                                        },
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
+
+    controllerutil.SetControllerReference(m, ingress, r.Scheme)
+    return ingress
+}
+
 
 // labelsForApp creates a simple set of labels for Memcached.
 func labelsForApp(name string) map[string]string {
